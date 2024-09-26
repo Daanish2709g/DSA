70d3cd0023452b1301cb1ccd5a18d587463b4b66
Daanish ilahi <daanishsumkesula@gmail.com>
2024-09-26 17:12:31 +0530

    Create Hashing.java

---
+++ Hashing.java
@@ -0,0 +1,22 @@
+/*Hashing is a technique used in data structures that efficiently stores and retrieves data in a way
+that allows for quick access.Hashing is a data structure technique that maps data 
+to fixed-size values, or hashes, to make data easier to find and access: 
+
+The great thing about hashing is, we can achieve all three operations
+(search, insert and delete) in O(1) time on average.
+
+How it works
+A hash function maps a key or string of characters to a hash value, which is then used as an 
+index to access data in a hash table. 
+  
+Why it's useful
+Hashing is a computationally and storage-efficient way to access data. It's used in many 
+applications, including data indexing and retrieval, digital signatures, cybersecurity, 
+and cryptography.
+  
+Examples
+This method is commonly used in databases, caching systems, and various programming applications 
+to optimize search and retrieval operations.
+
+Hashing is used in universities to assign students unique roll numbers, 
+and in libraries to assign books unique numbers.*/
