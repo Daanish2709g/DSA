3e38ae715b55c6ab04df9f22ace7df749d4fca22
Daanish ilahi <daanishsumkesula@gmail.com>
2024-09-29 03:15:46 +0530

    Create Kth Missing Positive Number in a Sorted Array.java

---
+++ Array/Kth Missing Positive Number in a Sorted Array.java
@@ -0,0 +1,21 @@
+/*Given an increasing sequence a[], we need to find the K-th missing contiguous
+element in the increasing sequence which is not present in the sequence.
+
+Examples : 
+
+Input : a[] = {2, 3, 5, 9, 10},   k = 1;
+Output : 1
+Explanation: Missing are 1,4, 6, 7, 8, 11, … So k-th missing element  is 1
+
+
+Input : a[] = {1, 2, 3},   k = 2;
+Output : 5
+Explanation: Missing are 4, 5, 6…. So k-th missing element  is 1
+
+
+Input : a[] = {2, 3, 5, 9, 10, 11, 12},  k = 4;
+Output : 7
+Explanation: Missing are 1, 4, 6, 7, 8, 13,…  so k-th missing element is 7*/
+
+
+
