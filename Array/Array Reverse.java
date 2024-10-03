8b4929fee2db58d70ccb157985d7183a6af24ddd
Daanish ilahi <daanishsumkesula@gmail.com>
2024-10-04 02:15:48 +0530

    Update Array Reverse.java

--- Array/Array Reverse.java
+++ Array/Array Reverse.java
@@ -88,3 +88,24 @@ public class Practise{
 its corresponding element from the end. So, while iterating over the first half, any 
   element at index i is swapped with the element at index (n – i – 1).*/
 
+public class Practise{
+	public static void reverseArray(int[] arr) {
+		int n = arr.length;
+		
+		for (int i = 0; i<n/2; i++) {
+			int temp = arr[i];
+			arr[i] = arr[n-i-1];
+			arr[n-i-1] = temp;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 5};
+		
+		reverseArray(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+ " ");
+		}
+	}
+}
